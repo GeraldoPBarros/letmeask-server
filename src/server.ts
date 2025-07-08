e65fca9a780b17b79cca0b78dc0530099ed8e3aa
BarrosNB <69353700+BarrosNB@users.noreply.github.com>
2025-07-08 14:47:38 -0300

    initial commit.

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { fastify } from "fastify";
+
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import { fastifyCors } from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5173", // or * to accept any URL
+});
+
+app.setSerializerCompiler(serializerCompiler);
+
+app.setValidatorCompiler(validatorCompiler);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running!");
+});
